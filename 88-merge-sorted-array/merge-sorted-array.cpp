d3599633cfc3f9c85a1380a02187191d286e77ed
gayatri-tonde <164787571+gayatri-tonde@users.noreply.github.com>
2025-07-30 22:45:14 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (70.47%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cpp
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -1,14 +1,27 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        for (int j = 0; j < n; j++) {
-            int i = m - 1;
-            while (i >= 0 && nums2[j] < nums1[i + j]) {
-                nums1[i + j + 1] = nums1[i + j];
-                i--;
+        // for (int j = 0; j < n; j++) {
+        //     int i = m - 1;
+        //     while (i >=  && nums2[j] < nums1[i + j]) {
+        //         nums1[i + j + 1] = nums1[i + j];
+        //         i--;
+        //     }
+        //     // when j is not smaller
+        //     nums1[i + j + 1] = nums2[j];
+        // }
+
+        //optimised code
+        int i = m - 1;
+        int j = n - 1;
+        int k = m + n - 1;
+        
+        while (j >= 0) {
+            if (i >= 0 && nums1[i] > nums2[j]) {
+                nums1[k--] = nums1[i--];
+            } else {
+                nums1[k--] = nums2[j--];
             }
-            // when j is not smaller
-            nums1[i + j + 1] = nums2[j];
         }
     }
 };
