5f9fc2042feb3ba36cd6d76c34e50923ad120b91
gayatri-tonde <164787571+gayatri-tonde@users.noreply.github.com>
2025-07-30 22:30:55 +0530

    Time: 0 ms (100.00%) | Memory: 12.1 MB (95.66%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cpp
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -4,11 +4,11 @@ public:
         for (int j = 0; j < n; j++) {
             int i = m - 1;
             while (i >= 0 && nums2[j] < nums1[i + j]) {
-                nums1[i + 1 + j] = nums1[i + j];
+                nums1[i + j + 1] = nums1[i + j];
                 i--;
             }
             // when j is not smaller
-            nums1[i + 1 + j] = nums2[j];
+            nums1[i + j + 1] = nums2[j];
         }
     }
 };
