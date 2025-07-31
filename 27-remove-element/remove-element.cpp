cd7a359ba98204a4d8ab0024a67ede08e8afc8aa
gayatri-tonde <164787571+gayatri-tonde@users.noreply.github.com>
2025-07-31 17:35:13 +0530

    Time: 0 ms (100.00%) | Memory: 11.9 MB (5.60%) - LeetSync

---
+++ 27-remove-element/remove-element.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        // int k = nums.size() - 1;
+        //  int i = 0;
+        //  int j = nums.size() - 1;
+        //  while (i < j) {
+        //      while (nums[i] == val) {
+        //          if (nums[j] != val) {
+        //              swap(nums[i], nums[j]);
+        //              k--;
+        //          }
+        //          j--;
+        //      }
+        //      i++;
+        //  }
+        int i = 0;
+        int j = 0;
+        int k = 0;
+        while (i < nums.size()) {
+            if (nums[i] != val) {
+                ++k;
+                nums[j++] = nums[i];
+            }
+            i++;
+        }
+        return k;
+    }
+};
