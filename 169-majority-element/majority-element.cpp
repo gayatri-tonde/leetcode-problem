99b3ebe333213f999d947f06ae67b689410253c2
gayatri-tonde <164787571+gayatri-tonde@users.noreply.github.com>
2025-08-01 20:56:41 +0530

    Time: 9 ms (8.26%) | Memory: 28.1 MB (86.41%) - LeetSync

---
+++ 169-majority-element/majority-element.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        int i = 0;
+        int j = 1;
+        int k = 0;
+        while (k < n / 2 && i < n) {
+            if (nums[i] == nums[j] && j < n) {
+                k++;
+                j++;
+            }
+            else {
+                k = 0;
+                i++;
+            }
+        }
+        return nums[i];
+    }
+};
