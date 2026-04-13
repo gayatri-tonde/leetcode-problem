623ecd2ba1464652c6e8c47cfc37a332c8be78c9
gayatri-tonde <164787571+gayatri-tonde@users.noreply.github.com>
2026-04-13 12:56:10 +0530

    Time: 0 ms (100.00%) | Memory: 48.1 MB (86.82%) - LeetSync

---
+++ 792-binary-search/binary-search.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int search(int[] nums, int target) {
+
+        int start = 0;
+        int end = nums.length - 1;
+
+        while(start <= end){
+            int n = (start + end) / 2;
+
+            if(target == nums[n]){
+                return n;
+            }
+
+            if(target < nums[n]){
+                end = n - 1;
+            }
+
+            if(target > nums[n]){
+                start = n + 1;
+            }
+        }
+        return -1;
+    }
+}
